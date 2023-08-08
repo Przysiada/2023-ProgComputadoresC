5691813fa136264e8bb32b991f04dc8b39c8b96b
Przysiada <56591954+Przysiada@users.noreply.github.com>
2023-08-08 14:48:30 -0300

    Create Exemplo01.c

---
+++ Aula02/Exemplo01.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int somaNumeros ( int n )
+{
+	if ( n <= 1 ) {
+		return n;
+	}
+	else {
+		return (n + somaNumeros( n - 1) );
+	}
+}
+
+void main()
+{
+	int n, soma;
+
+	printf( "Digite o valor do numero a ser somado = ");
+	scanf( "%d" , &n );
+
+	soma = somaNumeros(n);
+
+	printf( "O valor da soma = %d", soma);
+}
